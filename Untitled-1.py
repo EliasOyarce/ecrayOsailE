f88496295751f4a69f000f5b5ba06f729935d628
EliasOyarce <el.sanmartin@duocuc.cl>
2025-06-25 19:47:55 -0400

    ReReupdate

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,24 @@
-print("Wena Frope Profe forpef ferpof")
-print("kskjdhsdjfbjawsbfhjsdnkdwnaa")
-print("ksjdkljsadkpsa")
+# Lista de alumnos Duoc UC ses
+
+Alumnos_AP = [
+    [
+        {
+            "Nombre": ["Benjamin","Benjamin","Amaro"]
+        }
+    ],[
+        {
+            "Apellido": ["Andrade","Andres","Barria"]
+        }
+    ],[
+        {
+            "Correo": ["ben.andrade@duocuc.cl","ben.astudillo@duocuc.cl","am.barria@duocuc.cl"]
+        }
+    ]
+]
+
+Nombre = Alumnos_AP[0][0]["Nombre"]
+Apellido = Alumnos_AP[1][0]["Apellido"]
+Correo = Alumnos_AP[2][0]["Correo"]
+
+for nom, ape, mem in zip(Nombre, Apellido, Correo):
+    print(f"{nom} {ape}, Correo: {mem}")
