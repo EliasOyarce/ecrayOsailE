e3c7ac1b44a27cabcd1d08d597a40eccfba190ea
EliasOyarce <el.sanmartin@duocuc.cl>
2025-06-26 09:10:07 -0400

    sas

--- numeros = [[[1,2,3,4,5]],[[6,7,8,9,10]],.py
+++ numeros = [[[1,2,3,4,5]],[[6,7,8,9,10]],.py
@@ -1,2 +1,5 @@
 numeros = [[[1,2,3,4,5]],[[6,7,8,9,10]],[[11,12,13,14,15]]]
+
 print(sorted((numeros[0][0])+(numeros[1][0])+(numeros[2][0])))
+print(sorted((numeros[0][0])+(numeros[1][0])+(numeros[2][0]), reverse=True))
+
