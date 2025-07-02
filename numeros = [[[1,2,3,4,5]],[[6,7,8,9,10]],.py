88c249b157e2c49c8cc6b18618b4d7138750adbd
EliasOyarce <el.sanmartin@duocuc.cl>
2025-07-01 21:37:44 -0400

    Numeros Definitivo

--- numeros = [[[1,2,3,4,5]],[[6,7,8,9,10]],.py
+++ numeros = [[[1,2,3,4,5]],[[6,7,8,9,10]],.py
@@ -1,20 +1,59 @@
-numeros = [[[1,2,3,4,5]],[[6,7,8,9,10]],[[11,12,13,14,15]]]
+from os import system
+system("cls")
 
-print("\n",sorted((numeros[0][0])+(numeros[1][0])+(numeros[2][0])))
-print(sorted((numeros[0][0])+(numeros[1][0])+(numeros[2][0]), reverse=True),"\n")
+Programa_funcionando = True
 
-def agregar_num(lista : list):
-    list1 = []
-    list2 = []
-    cant = int(input("cuant numeros agregar: "))
+numeros = []
+cont = 1  
+
+def agregar_num(lista: list, cont: int) -> int:
+    list1 = [f"jugador {cont}", []]
+    try:
+        cant = int(input("Cuantos numeros desea agregar?: "))
+    except ValueError:
+        print("Ingresa un número válido.")
+        return cont
     for i in range(cant):
-        num_mum = int(input("ingrese los numeros que desea agregar: "))
-        list1.append(num_mum)
-        print("numero agregado exitosamente.")
-    list1.append(list2)
-    return lista.append(list2)
+        try:
+            num_mum = int(input("Ingresa el número que deseas agregar: "))
+        except ValueError:
+            print("Valor inválido, intenta de nuevo.")
+            continue
+        list1[-1].append(num_mum)
+        print("Número agregado exitosamente.")
+    lista.append(list1)
+    return cont + 1  
 
-agregar_num(numeros)
+def ver_numeros(lista: list) -> bool:
+    if not lista:
+        print("No se ingresaron números")
+        return False
+    else:
+        print("\nListado de jugadores y sus números:")
+        for jugador in lista:
+            nombre = jugador[0]
+            nums = jugador[1]
+            print(f"{nombre}: {', '.join(map(str, nums))}")
+    return True
 
-print("\n",sorted((numeros[0][0])+(numeros[1][0])+(numeros[2][0])))
-print(sorted((numeros[0][0])+(numeros[1][0])+(numeros[2][0]), reverse=True),"\n")
+while Programa_funcionando:
+    print("="*20,"MENÚ","="*20)
+    print("A) Agregar números.")
+    print("B) Ver números.")
+    print("X) Salir del programa.")
+    try:
+        opc = input("Elija una opción: ").strip().upper()
+    except:
+        print("Hubo un error.")
+        continue
+
+    if opc == "A":
+        cont = agregar_num(numeros, cont)
+    elif opc == "B":
+        ver_numeros(numeros)
+    elif opc == "X":
+        Programa_funcionando = False
+    else:
+        print("Opción inválida, intenta nuevamente.")
+
+print("Saliendo del programa.")
